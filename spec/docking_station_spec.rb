a30e5b2cf5dd16337ec7ee038b49843be2fd5d43
Mike Field-May <mfieldmay@gmail.com>
2017-01-05 13:07:27 +0000

    Add further test to docking station and set working? method in Bike class to be true

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,6 @@ require 'docking_station'
 
 describe DockingStation do
   it { expect(subject).to respond_to('release_bike') }
+  it { expect(subject.release_bike).to be_a Bike }
+  it { expect(subject.release_bike.working?).to eq true }
 end
