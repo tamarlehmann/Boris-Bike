c477c0fce3daa608c5371e195f838d044a90ec8e
Irene Canuti <irene.canuti@gmail.com>
2017-01-06 12:39:15 +0000

    Raised exception in the code, passed feature and unit test with raise_error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,18 @@
 class DockingStation
   attr_reader :bike
 
-  def initialize
-    @bike = Bike.new
-    @bike_count = 1
-  end
+  # def initialize
+  #   @bike = Bike.new
+  #   @bike_count = 1
+  # end
 
   def release_bike
-    raise "No bikes available" unless @bike_count > 0
-    @bike_count -= 1
-    Bike.new
+    raise "No bikes available" unless @bike
+    return @bike
   end
 
-  def dock_bike
+  def dock_bike(bike)
+    @bike = bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,30 @@ require 'docking_station'
 
 describe DockingStation do
   it { expect(subject).to respond_to('release_bike') }
-  it { expect(subject.release_bike).to be_a Bike }
-  it { expect(subject.release_bike.working?).to eq true }
+
+  it 'release a bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.release_bike).to eq bike
+  end
+
+  # it { expect(subject.release_bike.working?).to eq true }
   it { expect(subject).to respond_to('dock_bike')}
-  it { expect(subject.bike).to be_a Bike}
-  it { expect{raise "No bikes available"}.to raise_error("No bikes available") }
+  it { expect(subject.bike).to eq @bike}
+#   it { expect{raise "No bikes available"}.to raise_error("No bikes available") }
+
+  it 'raise an error when no bikes available' do
+    expect{subject.release_bike}.to raise_error("No bikes available")
+  end
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+
+
+  # it 'raises an error' do
+  #   expect{subject.release_bike(nil)}.to raise_error("No bikes available")
+  # end
+
 end
