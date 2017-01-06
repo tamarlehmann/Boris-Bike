01d7f936b7b12d7d165582218428c080e0459d17
Irene Canuti <irene.canuti@gmail.com>
2017-01-06 15:46:46 +0000

    Applied single responsability principle and set new methods to private

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,13 +6,36 @@ class DockingStation
   end
 
   def release_bike
-    raise "No bikes available" unless @bikes.count > 0
+    if empty?
+    raise "No bikes available"
+    else
     return @bikes.pop
+    end
   end
 
   def dock_bike(bike)
-    raise "No space available" unless @bikes.count < 20
-    @bikes << bike
+    if full?
+      raise "No space available"
+    else
+      @bikes << bike
+    end
+  end
+
+private
+  def empty?
+    if @bikes.empty?
+      return true
+    else
+      return false
+    end
+  end
+
+  def full?
+    if @bikes.count >= 20
+      return true
+    else
+      return false
+    end
   end
 
 end
